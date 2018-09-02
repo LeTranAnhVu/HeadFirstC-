2598f0a1193893d798be02f2e1d1b179327fc13a
Vu_Le <vule5726@gmail.com>
2018-09-02 19:52:01 +0700

    contact app 100%

--- contact/contact/Form1.cs
+++ contact/contact/Form1.cs
@@ -16,5 +16,41 @@ namespace contact
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact List 1.0.\nWritten by Vu Le", "About");
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click_2(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'contactDBDataSet.People' table. You can move, or remove it, as needed.
+            this.peopleTableAdapter.Fill(this.contactDBDataSet.People);
+
+        }
     }
 }
